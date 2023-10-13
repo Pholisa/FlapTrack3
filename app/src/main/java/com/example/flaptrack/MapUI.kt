ae21138d7a9635bcc0b3b3d5b33a4e8dfbb0a6cd
Juliet Mukungwa <mukungwaj@gmail.com>
2023-10-13 09:50:15 +0200

    updated maps api requests and displaying hotspots from a specific km. I will make the feature more dynamic once the set max distance feature is set up fully in settings. for now it uses a constant km of 10 that i hard coded.

--- app/src/main/java/com/example/flaptrack/MapUI.kt
+++ app/src/main/java/com/example/flaptrack/MapUI.kt
@@ -6,7 +6,6 @@ import android.location.Location
 import android.os.Bundle
 import androidx.core.app.ActivityCompat
 import androidx.fragment.app.FragmentActivity
-import com.example.flaptrack.databinding.ActivityAboutBinding
 import com.example.flaptrack.databinding.ActivityMapUiBinding
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
@@ -16,6 +15,11 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import org.json.JSONArray
+import org.json.JSONException
+import org.json.JSONObject
+import kotlin.concurrent.thread
+import kotlin.math.*
 
 class MapUI : FragmentActivity(), OnMapReadyCallback {
 
@@ -23,22 +27,41 @@ class MapUI : FragmentActivity(), OnMapReadyCallback {
     private lateinit var fusedLocationClient: FusedLocationProviderClient
     private val LOCATION_PERMISSION_REQUEST = 1
 
+    var hotspotList = mutableListOf<HotspotData>()
+
     private lateinit var binding: ActivityMapUiBinding
+    private var userLocation: LatLng = LatLng(0.0, 0.0) // Initialize with a default value
+    private val maxDistanceKm = 10.0 // Set the maximum distance in kilometers
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //setContentView(R.layout.activity_map_ui)
         binding = ActivityMapUiBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        //calling the navigation bar function
+
+        // Initialize NetworkUtil
+        val networkUtil = NetworkUtil()
+
+        // Calling the navigation bar function
         navigationBar()
 
-        val mapFragment = supportFragmentManager.findFragmentById(R.id.google_map) as SupportMapFragment
+        val mapFragment =
+            supportFragmentManager.findFragmentById(R.id.google_map) as SupportMapFragment
         mapFragment.getMapAsync(this)
 
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
 
+        thread {
+            val hotspot = try {
+                networkUtil.buildURLForEbird()?.readText()
+            } catch (e: Exception) {
+                // Handle the error, e.g., display a message to the user
+                return@thread
+            }
 
+            runOnUiThread {
+                consumeJson(hotspot)
+            }
+        }
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
@@ -57,46 +80,111 @@ class MapUI : FragmentActivity(), OnMapReadyCallback {
         fusedLocationClient.lastLocation
             .addOnSuccessListener { location: Location? ->
                 location?.let {
+                    // Update user's location
+                    userLocation = LatLng(it.latitude, it.longitude)
+
                     // Add a marker at the device's current location
-                    val userLocation = LatLng(it.latitude, it.longitude)
                     val markerOptions = MarkerOptions()
                     markerOptions.position(userLocation)
                     mMap.addMarker(markerOptions)
 
                     // Move the camera to the device's current location
                     mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(userLocation, 15f))
+
+                    // Filter and display hotspots
+                    filterAndDisplayHotspots()
                 }
             }
     }
 
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if (requestCode == LOCATION_PERMISSION_REQUEST && grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-            // Permission granted, show the device's current location on the map
-            onMapReady(mMap)
+    private fun filterAndDisplayHotspots() {
+        val filteredHotspots = hotspotList.filter { hotspot ->
+            val distance = calculateDistance(userLocation, LatLng(hotspot.lat ?: 0.0, hotspot.lng ?: 0.0))
+            distance <= maxDistanceKm
         }
+
+
+        filteredHotspots.forEach { hotspot ->
+            val lat = hotspot.lat ?: 0.0
+            val lng = hotspot.lng ?: 0.0
+            val hotspotLocation = LatLng(lat, lng)
+            val markerOptions = MarkerOptions()
+                .position(hotspotLocation)
+                .title(hotspot.locName)
+            mMap.addMarker(markerOptions)
+        }
+
     }
 
-    private fun navigationBar()
-    {
-        //This will account for event clicking of the navigation bar (similar to if statement format)
-        binding.bottomNavigationView.setOnItemSelectedListener {item ->
+    // Haversine formula to calculate distance between two points
+    private fun calculateDistance(p1: LatLng, p2: LatLng): Double {
+        val lat1 = p1.latitude
+        val lon1 = p1.longitude
+        val lat2 = p2.latitude
+        val lon2 = p2.longitude
+        val r = 6371.0 // Earth's radius in kilometers
+        val latDistance = Math.toRadians(lat2 - lat1)
+        val lonDistance = Math.toRadians(lon2 - lon1)
+        val a = sin(latDistance / 2) * sin(latDistance / 2) +
+                cos(Math.toRadians(lat1)) * cos(Math.toRadians(lat2)) *
+                sin(lonDistance / 2) * sin(lonDistance / 2)
+        val c = 2 * atan2(sqrt(a), sqrt(1 - a))
+        return r * c
+    }
+
+    private fun navigationBar() {
+        binding.bottomNavigationView.setOnItemSelectedListener { item ->
             when (item.itemId) {
                 R.id.idBirds -> {
                     val intent = Intent(this, BirdsUi::class.java)
-                    startActivity(intent)
-                }
-                R.id.idHome -> {
-                    val intent = Intent(this, MapUI::class.java)
                     startActivity(intent)
                 }
                 R.id.idAccount -> {
                     val intent = Intent(this, AccountSettings::class.java)
                     startActivity(intent)
                 }
-                else -> {}
+                else -> {
+                    // Handle other cases as needed
+                }
             }
             true
+        }
+    }
+
+    private fun consumeJson(hotspotJSON: String?) {
+        if (hotspotJSON != null) {
+            try {
+                val rootHotspotData = JSONArray(hotspotJSON)
+
+                for (i in 0 until rootHotspotData.length()) {
+                    val hotspotObject = HotspotData()
+                    val hotspot = rootHotspotData.getJSONObject(i)
+
+                    if (hotspot.has("locName")) {
+                        hotspotObject.locName = hotspot.getString("locName")
+                    }
+
+                    if (hotspot.has("lat")) {
+                        hotspotObject.lat = hotspot.getDouble("lat")
+                    }
+
+                    if (hotspot.has("lng")) {
+                        hotspotObject.lng = hotspot.getDouble("lng")
+                    }
+
+                    // Add the hotspotObject to the list
+                    hotspotList.add(hotspotObject)
+                }
+
+            } catch (e: JSONException)
+            {
+                // Handle JSON parsing errors
+                e.printStackTrace()
+            }
+        }
+        else
+        {
+            // Handle the case where hotspotJSON is null
         }
     }
 }
