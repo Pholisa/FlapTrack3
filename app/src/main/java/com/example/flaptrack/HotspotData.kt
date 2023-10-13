6c77eda7d57cbf351064482a99cbe241a587bdc7
Juliet Mukungwa <mukungwaj@gmail.com>
2023-10-13 10:14:25 +0200

    updated maps api requests and displaying hotspots from a specific km. I will make the feature more dynamic once the set max distance feature is set up fully in settings. for now it uses a constant km of 10 that i hard coded.

---
+++ app/src/main/java/com/example/flaptrack/HotspotData.kt
@@ -0,0 +1,7 @@
+package com.example.flaptrack
+
+class HotspotData {
+    var locName           : String? = null
+    var lat               : Double? = null
+    var lng               : Double? = null
+}
---
+++ app/src/main/java/com/example/flaptrack/NetworkUtil.kt
@@ -0,0 +1,37 @@
+package com.example.flaptrack
+
+import android.net.Uri
+import android.util.Log
+import java.net.MalformedURLException
+import java.net.URL
+
+private val EBIRD_BASEURL = "https://api.ebird.org/v2/ref/hotspot/ZA?fmt=json"
+private val PARAM_METRIC = "metric"
+private val METRIC_VALUE = "true"
+private val PARAM_API_KEY = "key"
+private val LOGGING_TAG = "URLICREATED"
+
+class NetworkUtil
+{
+    fun buildURLForEbird(): URL? {
+        val buildUri: Uri = Uri.parse(EBIRD_BASEURL).buildUpon()
+            .appendQueryParameter(
+                PARAM_API_KEY,
+                BuildConfig.EBIRD_API_KEY
+            ) // passing in api key
+            .appendQueryParameter(
+                PARAM_METRIC,
+                METRIC_VALUE
+            ) // passing in metric as measurement unit
+            .build()
+        var url: URL? = null
+        try {
+            url = URL(buildUri.toString())
+        } catch (e: MalformedURLException) {
+            e.printStackTrace()
+        }
+        Log.i(LOGGING_TAG, "buildURLForEbird: $url")
+        return url
+    }
+}
+
